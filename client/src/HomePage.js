f0118a4415fbd8c81ef6a9401bcd05fd2958c304
lynden <lyndenlim15@gmail.com>
2022-04-07 00:46:57 -0400

    Display nearby restaurants

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import getRestaurantData from "./RandomRestaurant"
+import React, { useState } from 'react'
+import { Link, useHistory } from "react-router-dom"
 
-function HomePage() {
+function HomePage({ setAddress }) {
+    const [userInput, setUserInput] = useState("")
+    const history = useHistory()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setAddress(userInput)
+        history.push({ pathname: "/restaurants" })
+    }
+
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="Enter an address" defaultValue={userInput} onChange={e => setUserInput(e.target.value)}></input>
+                <button type="submit">Search</button>
+            </form>
+            <br />
+            <br />
             <Link to="/random">
                 <button>CHOOSE FOR ME</button>
             </Link>
