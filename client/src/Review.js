22e559c63b68371a6486e48bcce39c407d51472a
lynden <lyndenlim15@gmail.com>
2022-04-07 14:42:36 -0400

    Build review page

--- client/src/Review.js
+++ client/src/Review.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Review() {
+function Review({ review }) {
+  const [dateCreated, setDateCreated] = useState("")
+
+  useEffect(() => {
+    const splitDate = review.time_created.split(" ")[0].split("-")
+    const reformattedDate = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
+    setDateCreated(reformattedDate)
+  }, [])
+
   return (
-    <div>Review</div>
+    <>
+      <img height="100px" width="100px" src={review.user.image_url !== null ? review.user.image_url : "https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"} alt="user-avatar" /><h5>{review.user.name}</h5>
+      <p>{dateCreated}</p>
+      <p>{review.rating}</p>
+      <p>{review.text}</p>
+      <hr />
+    </>
   )
 }
 
