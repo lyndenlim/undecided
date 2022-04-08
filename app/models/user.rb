317a70db691cb3c096f7a8592112e19c5af10c13
lynden <lyndenlim15@gmail.com>
2022-04-07 21:14:28 -0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :reviews
 
   has_secure_password
+
+  attr_accessor :old_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
