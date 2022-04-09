1bdee72d3ccfe56c31d42db3b11b4f5c55494d11
lynden <lyndenlim15@gmail.com>
2022-04-08 22:38:32 -0400

    Style homepage

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,9 +1,26 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
-import { Link, useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 
 function HomePage({ setAddress }) {
     const history = useHistory()
     const [userInput, setUserInput] = useState("")
+    const [background, setBackground] = useState("")
+
+    useEffect(() => {
+        async function getPhotos() {
+            const axiosInstance = axios.create({
+                headers: {
+                    Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
+                }
+            })
+            const restaurantPhotos = await axiosInstance.get("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=20&latitude=40.7052671&longitude=-74.0139441&radius=805&open_now=true&categories=restaurants")
+            const photoArray = restaurantPhotos.data.businesses.map(business => business.image_url)
+            const randomPhoto = Math.ceil(Math.random(photoArray) * photoArray.length)
+            setBackground(photoArray[randomPhoto])
+        }
+        getPhotos()
+    }, [])
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -13,15 +30,17 @@ function HomePage({ setAddress }) {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input placeholder="Enter an address" defaultValue={userInput} onChange={e => setUserInput(e.target.value)}></input>
-                <button type="submit">Search</button>
-            </form>
-            <br />
-            <br />
-            <Link to="/random">
-                <button>CHOOSE FOR ME</button>
-            </Link>
+            {/* revisit */}
+            <img src={background} alt="background" className="homepage-background"/>
+            <div className="homepage-container">
+                <form onSubmit={handleSubmit}>
+                    <input placeholder="Enter an address" defaultValue={userInput} onChange={e => setUserInput(e.target.value)} required></input>
+                    <button type="submit">Search</button>
+                </form>
+                <br />
+                <br />
+                <button className="search-button bouncy" onClick={() => history.push("/random")}>CHOOSE FOR ME</button>
+            </div>
         </>
     )
 }
