59ada82ca956a3fcd356f3ab60ff185672a4db07
lynden <lyndenlim15@gmail.com>
2022-04-10 13:31:45 -0400

    Style account page

--- client/src/AccountReview.js
+++ client/src/AccountReview.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from "react-router-dom"
 
 function AccountReview({ review }) {
     const [name, setName] = useState("")
+    const [dateCreated, setDateCreated] = useState("")
 
     useEffect(() => {
         async function getRestaurantName() {
@@ -11,15 +13,22 @@ function AccountReview({ review }) {
                     Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
                 }
             })
-            const restaurantName = await axiosInstance.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${review.restaurant_id}`)
-            setName(restaurantName.data.name)
+            const restaurantInfo = await axiosInstance.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${review.restaurant_id}`)
+            const dateSplit = review.created_at.split("T")[0].split("-")
+            const reformattedDate = `${dateSplit[1]}/${dateSplit[2]}/${dateSplit[0]}`
+
+            setName(restaurantInfo.data.name)
+            setDateCreated(reformattedDate)
         }
         getRestaurantName()
     }, [])
 
     return (
         <div>
-            <h4>{name}</h4>
+            <Link to={`/restaurants/${review.restaurant_id}`} className="link">
+                <h4>{name}</h4>
+            </Link>
+            <p>{dateCreated}</p>
             <p>{review.rating}</p>
             <p>{review.comment}</p>
             <hr />
