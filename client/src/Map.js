445b7cef8d2c3abdce772092fe890a976212022f
lynden <lyndenlim15@gmail.com>
2022-04-09 21:26:09 -0400

    Style restaurant info page

--- client/src/Map.js
+++ client/src/Map.js
@@ -5,8 +5,8 @@ function Map({ restaurantAddress }) {
         <>
             <iframe
                 title="map"
-                width="600"
-                height="450"
+                width="500"
+                height="350"
                 style={{ border: "0" }}
                 loading="lazy"
                 src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_API_KEY}&q=${restaurantAddress}`}
