ee5b804e7388e0730bba85ab87806fbc2ad2f4ac
lynden <lyndenlim15@gmail.com>
2022-04-11 22:14:30 -0400

    Style restaurant cards

--- client/src/RestaurantCard.js
+++ client/src/RestaurantCard.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Card from "react-bootstrap/Card"
 
 function RestaurantCard({ restaurant }) {
     return (
         <div className="restaurant-card">
             <Link to={`/restaurants/${restaurant.id}`} className="link">
-                {/* revisit, uniform card formatting */}
-                <Card>
-                    <Card.Header>
-                        <h4>
-                            <strong>
-                                {restaurant.name}
-                            </strong>
-                        </h4>
-                    </Card.Header>
-                    <Card.Img height="300px" width="300px" src={restaurant.image_url} alt="restaurant-images" />
-                    <Card.Body>
-                        <p>{restaurant.location.display_address[0]} {restaurant.location.display_address[1]}</p>
-                        <p>{(restaurant.distance / 1609).toFixed(2)} miles</p>
-                        <p>{restaurant.rating} {restaurant.review_count} reviews</p>
-                        <div><strong>{restaurant.price}</strong> • {restaurant.categories.map(category => <p key={category.title} className="category">{category.title}</p>)}</div>
-                    </Card.Body>
-                </Card>
+                <div className="wrapper">
+                    <div className="cols">
+                        <div className="col">
+                            <div className="container">
+                                <div className="front" style={{ backgroundImage: `url(${restaurant.image_url})` }}>
+                                    <div className="inner">
+                                        <p className="restaurant-card-restaurant-name">{restaurant.name}</p>
+                                    </div>
+                                </div>
+                                <div className="back">
+                                    <div className="inner">
+                                        <p>{restaurant.location.display_address[0]} {restaurant.location.display_address[1]}</p>
+                                        <p>{(restaurant.distance / 1609).toFixed(2)} miles</p>
+                                        <p>{restaurant.rating} {restaurant.review_count} reviews</p>
+                                        <div>{restaurant.price} • {restaurant.categories.map(category => <p key={category.title} className="category">{category.title}</p>)}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Link>
         </div>
     )
