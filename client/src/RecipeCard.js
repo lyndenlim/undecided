72c4eff4f200052834daa4310458979aef7c3d8b
lynden <lyndenlim15@gmail.com>
2022-04-11 23:20:09 -0400

    Style recipe cards

--- client/src/RecipeCard.js
+++ client/src/RecipeCard.js
@@ -8,39 +8,46 @@ import ListGroupItem from 'react-bootstrap/esm/ListGroupItem'
 function RecipeCard({ recipe }) {
     const [recipeInfo, setRecipeInfo] = useState([])
     const [recipeCuisines, setRecipeCuisines] = useState([])
-    const [recipeDiets, setRecipeDiets] = useState([])
 
     useEffect(() => {
         async function getRecipeInfo() {
             const recipeInformation = await axios.get(`https://api.spoonacular.com/recipes/${recipe.id}/information?apiKey=${process.env.REACT_APP_SPOONACULAR}`)
             setRecipeInfo(recipeInformation.data)
             setRecipeCuisines(recipeInformation.data.cuisines)
-            setRecipeDiets(recipeInformation.data.diets)
         }
 
         getRecipeInfo()
     }, [])
 
     return (
-        <>
-            <Card className="recipe-card">
-                <Link to={`/recipes/${recipe.id}`} className="link">
-                    <Card.Header className="recipe-header">{recipe.title}</Card.Header>
-                    <Card.Img src={recipe.image} className="recipe-image" />
-                    <Card.Body>Takes <strong>{recipeInfo.readyInMinutes}</strong> minutes to make</Card.Body>
-                    Ingredients not on hand:
-                    <ListGroup className="list-group-flush">
-                        <ListGroupItem>{recipe.missedIngredients.map((ingredient, index) => <ListGroupItem key={index}>{ingredient.original}</ListGroupItem>)}</ListGroupItem>
-                    </ListGroup>
-                    <Card.Body>
-                        {/* revisit */}
-                        <strong>Cuisine(s):</strong> {recipeCuisines.map((cuisine, index) => <p key={index} className="category">{cuisine}</p>)}
-                        <br />
-                        <strong>Diet Type(s):</strong>  {recipeDiets.map((diet, index) => <p key={index} className="category">{diet}</p>)}
-                    </Card.Body>
-                </Link>
-            </Card >
-        </>
+        <div className="recipe-card">
+            <Link to={`/recipes/${recipe.id}`} className="link">
+                <div className="wrapper">
+                    <div className="cols">
+                        <div className="col">
+                            <div className="container">
+                                <div className="front" style={{ backgroundImage: `url(${recipe.image})` }}>
+                                    <div className="inner">
+                                        <p className="recipe-card-recipe-name">{recipe.title}</p>
+                                        <div className="recipe-card-info">Takes <strong>{recipeInfo.readyInMinutes}</strong> minutes to make</div>
+                                        <strong className="recipe-card-info">Cuisine(s):</strong> {recipeCuisines.map((cuisine, index) => <p key={index} className="category recipe-card-info">{cuisine}</p>)}
+                                        <br />
+                                    </div>
+                                </div>
+                                <div className="back">
+                                    <div className="inner recipe-card-back">
+                                        <p style={{fontSize: "20px"}}>Missing ingredients:</p>
+                                        <ListGroup className="list-group-flush" style={{ borderRadius: "20px" }} >
+                                            {recipe.missedIngredients.map((ingredient, index) => <ListGroupItem key={index}>{ingredient.original}</ListGroupItem>)}
+                                        </ListGroup>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </div >
     )
 }
 
