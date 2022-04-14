a87d0a87f28840cca4fe4f1e9769fb28f119c212
lynden <lyndenlim15@gmail.com>
2022-04-14 18:50:54 -0400

    Change fetch to backend

---
+++ app/controllers/yelps_controller.rb
@@ -0,0 +1,61 @@
+require "net/http"
+require "uri"
+require "json"
+
+class YelpsController < ApplicationController
+  def yelp_restaurants
+    url = URI("https://api.yelp.com/v3/businesses/search?limit=10&latitude=#{params[:locationLat]}&longitude=#{params[:locationLng]}&radius=1610&open_now=true&categories=restaurants")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer #{params[:api_key]}"
+
+    response = https.request(request)
+    JSON.parse(response.body)
+    render json: response.body
+  end
+
+  def yelp_random_restaurant
+    url = URI("https://api.yelp.com/v3/businesses/search?limit=50&latitude=#{params[:currentLat]}&longitude=#{params[:currentLng]}&radius=1610&open_now=true&categories=restaurants")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer #{params[:api_key]}"
+
+    response = https.request(request)
+    JSON.parse(response.body)
+    render json: response.body
+  end
+
+  def yelp_restaurant
+    url = URI("https://api.yelp.com/v3/businesses/#{params[:restaurant_id]}")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer #{params[:api_key]}"
+
+    response = https.request(request)
+    JSON.parse(response.body)
+    render json: response.body
+  end
+
+  def yelp_reviews
+    url = URI("https://api.yelp.com/v3/businesses/#{params[:restaurant_id]}/reviews")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer #{params[:api_key]}"
+
+    response = https.request(request)
+    JSON.parse(response.body)
+    render json: response.body
+  end
+end
